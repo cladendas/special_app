f38bf82d401eb69495e0e64bf5668e5bde8d3707
cladendas <rvdagi05@yandex.ru>
2025-06-26 19:41:57 +0300

    Update Main.hs
    Добавил комменты, убрал преобразование в [Lead]

--- Main.hs
+++ Main.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-} -- позволяет автоматически генерировать экземпляры классов типов (например, Generic) для пользовательских типов данных
+{-# LANGUAGE OverloadedStrings #-} -- позволяет использовать строковые литералы для разных строковых типов (Text, ByteString и др.)
 
 import Network.HTTP.Simple
 -- import Data.Aeson (Value, object, (.=))
@@ -15,6 +15,9 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Char8 as BSL
 
 -- Определяем тип данных, соответствующий структуре CSV
+-- автоматическое создание:
+-- Generic экземпляра для сериализации/десериализации
+-- Show экземпляра для преобразования в строку (для отладки)
 data Lead = Lead
   { created :: Text
   , name  :: Text
@@ -26,17 +29,18 @@ data Lead = Lead
 instance ToJSON Lead
 
 -- Делаем тип десериализуемым из CSV
-instance FromNamedRecord Lead
+instance FromNamedRecord Lead -- Эта строка говорит: "Создай автоматическую реализацию для парсинга CSV в Lead"
 
 main :: IO ()
 main = do
+    -- Чтение CSV-файла "input.csv" в ленивую байтовую строку
+    -- требуется, чтобы CSV-файл имел заголовки, соответствующие полям типа Lead
     csvData <- BL.readFile "input.csv"
 
     case decodeByName csvData of
       Left err -> putStrLn $ "Ошибка парсинга CSV: " ++ err
       Right (_, peopleVector) -> do
-        let peopleList = V.toList peopleVector
-        putStrLn $ "Успешно прочитано " ++ show (length peopleList) ++ " записей"
+        putStrLn $ "Успешно прочитано " ++ show (V.length peopleVector) ++ " записей"
 
-        let jsonData = encode (peopleList :: [Lead])
+        let jsonData = encode (peopleVector :: V.Vector Lead)
         BSL.putStrLn jsonData
